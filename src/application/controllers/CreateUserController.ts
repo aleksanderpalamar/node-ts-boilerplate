4bd75c1d2182e1f7b549597ed6c8b67f3a2a07ce
aleksanderpalamar <apalamardev@gmail.com>
2025-11-24 20:23:28 -0300

    feat: implement CreateUserController for user registration handling

---
+++ src/application/controllers/CreateUserController.ts
@@ -0,0 +1,19 @@
+import { container } from 'tsyringe';
+import { Request, Response } from 'express';
+import { CreateUserUseCase } from '@/core/usecase/CreateUserUseCase';
+
+export class CreateUserController {
+  async handle(req: Request, res: Response) {
+    const usecase = container.resolve(CreateUserUseCase);
+
+    const { name, email, password } = req.body;
+    try {
+      const user = await usecase.execute({ name, email, password });
+      return res.status(201).json(user);
+    } catch (err) {
+      const message =
+        err instanceof Error ? err.message : 'An unknown error occurred';
+      return res.status(400).json({ error: message });
+    }
+  }
+}
